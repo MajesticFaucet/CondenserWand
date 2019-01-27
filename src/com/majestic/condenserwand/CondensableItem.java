baf05beb5ee543d87214dc2d3f1f967676891943
MajesticFaucet <47028561+MajesticFaucet@users.noreply.github.com>
2019-01-27 17:26:38 -0500

    Added some comments

--- src/com/majestic/condenserwand/CondensableItem.java
+++ src/com/majestic/condenserwand/CondensableItem.java
@@ -2,14 +2,18 @@ package com.majestic.condenserwand;
 
 import org.bukkit.inventory.ItemStack;
 
+// Used to map each blockables' ratios for conversions between regular and blocked forms.
 public class CondensableItem {
 	private ItemStack base;
 	private ItemStack block;
 	private int ratio;
 	
 	public CondensableItem(ItemStack base, ItemStack block, int ratio) {
+		// base item
 		this.base = base;
+		// blocked form of item
 		this.block = block;
+		// ratio of base block to block form.
 		this.ratio = ratio;
 	}
 
