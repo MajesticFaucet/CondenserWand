8650938ae4e7a2edf80cf858a0df74a61b07dc29
MajesticFaucet <47028561+MajesticFaucet@users.noreply.github.com>
2019-01-29 23:01:09 -0500

    Added minecart chest support

--- src/com/majestic/condenserwand/listeners/ContainerClick.java
+++ src/com/majestic/condenserwand/listeners/ContainerClick.java
@@ -3,21 +3,27 @@ package com.majestic.condenserwand.listeners;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Chest;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.majestic.condenserwand.CondensableItem;
 import com.majestic.condenserwand.CondenserWand;
 import com.majestic.condenserwand.ConfigMgr;
-import com.majestic.condenserwand.InventoryManipulator;
 import com.majestic.condenserwand.PlayerTracker;
+import com.majestic.condenserwand.util.InventoryManipulator;
+import com.majestic.condenserwand.util.exceptions.NoPlayerPermException;
 
 public final class ContainerClick implements Listener {
 	
@@ -36,53 +42,118 @@ public final class ContainerClick implements Listener {
 		items.add(new CondensableItem(new ItemStack(Material.GOLD_NUGGET), new ItemStack(Material.GOLD_INGOT), 9));
 	}
 	
-	// TODO: Listen for entity clicks aswell
-	
 	// sorts chests, trapped chests, or enderchest when a player clicks on it with a wand and passes all checks
-	@EventHandler
-	public void onChestClick(PlayerInteractEvent e) {
+	@EventHandler(ignoreCancelled=true)
+	public static void onChestClick(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
-		if(!e.isCancelled() && e.hasBlock() && e.hasItem() && e.getItem().getType().equals(Material.getMaterial(ConfigMgr.getItemName()))) {
-			if(!ConfigMgr.isItemMetaCheck() || (e.getItem().hasItemMeta() && e.getItem().getItemMeta().hasDisplayName() && e.getItem().getItemMeta().getDisplayName().equals(ConfigMgr.getItemDisplayName()) && e.getItem().getItemMeta().hasLore() && e.getItem().getItemMeta().getLore().containsAll(ConfigMgr.getLore()))) {
-				PlayerTracker pt;
-				if(!PlayerTracker.getPlayers().containsKey(p)) {
-					pt = new PlayerTracker(p);
-				} else {
-					pt = PlayerTracker.getPlayers().get(p);
-				}
-				if(!ConfigMgr.isWandDelay() || pt.getLastTime() <= System.currentTimeMillis() - ConfigMgr.getWandDelay()) {
-					// WorldGuard permissions check
-					try {
-						if(!CondenserWand.getInstaceWorldGuard().canBuild(p, e.getClickedBlock())) {
-							// return if player doesn't have permission to build
-							pt.setLastTime();
-							return;
-						}
-					} catch(NoClassDefFoundError exception) {
-						// just ignore and carry on if worldguard isn't loaded
+		if(e.hasBlock() && e.hasItem() && e.getItem().getType().equals(Material.getMaterial(ConfigMgr.getItemName()))) {
+			if(!ConfigMgr.isItemMetaCheck() || metaCheck(e.getItem())) {
+				try {
+					accessCheckWithTime(p, e.getClickedBlock().getLocation());
+					
+					Material clickedblocktype = e.getClickedBlock().getType();
+					if(clickedblocktype.equals(Material.CHEST) || clickedblocktype.equals(Material.TRAPPED_CHEST)) {
+						Chest chest = (Chest) e.getClickedBlock().getState();
+						Inventory inv = chest.getInventory();
+						sift(inv, p);
+					} else if(clickedblocktype.equals(Material.ENDER_CHEST)) {
+						sift(p.getEnderChest(), p);
+					} else if(ConfigMgr.isSendWrongContainerMsg()) {
+						throw new NoPlayerPermException(ConfigMgr.getWrongContainerMsg());
 					}
-					if(p.hasPermission("condenserwand.use")) {
-						Material clickedblocktype = e.getClickedBlock().getType();
-						if(clickedblocktype.equals(Material.CHEST) || clickedblocktype.equals(Material.TRAPPED_CHEST)) {
-							Chest chest = (Chest) e.getClickedBlock().getState();
-							Inventory inv = chest.getInventory();
-							sift(inv, p);
-						} else if(clickedblocktype.equals(Material.ENDER_CHEST)) {
-							sift(p.getEnderChest(), p);
-						} else if(ConfigMgr.isSendWrongContainerMsg()) {
-							p.sendMessage(ConfigMgr.getWrongContainerMsg());
-						}
-					} else {
-						p.sendMessage(ConfigMgr.getDenyMsg());
+				} catch(NoPlayerPermException exception) {
+					String errmsg = exception.getMessage();
+					if(errmsg != null) {
+						p.sendMessage(errmsg);
 					}
-					pt.setLastTime();
+				} finally {
+					e.setCancelled(true);
 				}
-				e.setCancelled(true);
 			}
 		}
 	}
 	
-	private void sift(Inventory inv, Player p) {
+	// does everything the above method does except with minecart chests
+	@EventHandler(ignoreCancelled=true)
+	public static void onMinecartClick(PlayerInteractEntityEvent e) {
+		Player p = e.getPlayer();
+		if(e.getPlayer().getItemInHand() != null && e.getPlayer().getItemInHand().getType().equals(Material.getMaterial(ConfigMgr.getItemName()))) {
+			if(!ConfigMgr.isItemMetaCheck() || metaCheck(e.getPlayer().getItemInHand())) {
+				try {
+					accessCheckWithTime(p, e.getRightClicked().getLocation());
+					
+					EntityType clickedentitytype = e.getRightClicked().getType();
+					if(clickedentitytype.equals(EntityType.MINECART_CHEST)) {
+						StorageMinecart sm = (StorageMinecart) e.getRightClicked();
+						Inventory inv = sm.getInventory();
+						sift(inv, p);
+					} else if(ConfigMgr.isSendWrongContainerMsg()) {
+						throw new NoPlayerPermException(ConfigMgr.getWrongContainerMsg());
+					}
+				} catch (NoPlayerPermException exception) {
+					String errmsg = exception.getMessage();
+					if(errmsg != null) {
+						p.sendMessage(errmsg);
+					}
+				} finally {
+					e.setCancelled(true);
+				}
+			}
+		}
+	}
+	
+	// returns true if item has correct meta
+	private static boolean metaCheck(ItemStack is) {
+		if(is.hasItemMeta()) {
+			ItemMeta im = is.getItemMeta();
+			if(im.hasDisplayName() && im.getDisplayName().equals(ConfigMgr.getItemDisplayName()) && im.hasLore() && im.getLore().containsAll(ConfigMgr.getLore())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// performs some checks with a timer cooldown
+	private static void accessCheckWithTime(Player p, Location l) throws NoPlayerPermException {
+		PlayerTracker pt;
+		
+		if(!PlayerTracker.getPlayers().containsKey(p)) {
+			// creates new player tracker object if one doesn't exist
+			pt = new PlayerTracker(p);
+		} else {
+			pt = PlayerTracker.getPlayers().get(p);
+		}
+		
+		if(ConfigMgr.isWandDelay()) {
+			if(pt.getLastTime() > System.currentTimeMillis() - ConfigMgr.getWandDelay()) {
+				throw new NoPlayerPermException();
+			} else {
+				pt.setLastTime();
+			}
+		}
+		
+		accessCheck(p, l);
+		
+	}
+	
+	// performs some checks
+	private static void accessCheck(Player p, Location l) throws NoPlayerPermException {
+		// anti-grief plugin integration checks
+		try {
+			if(!CondenserWand.getInstaceWorldGuard().canBuild(p, l)) {
+				throw new NoPlayerPermException();
+			}
+		} catch(NoClassDefFoundError exception) {
+			// just ignore and carry on if WorldGuard isn't loaded
+		}
+		
+		
+		if(!p.hasPermission("condenserwand.use")) {
+			throw new NoPlayerPermException(ConfigMgr.getDenyMsg());
+		}
+	}
+	
+	private static void sift(Inventory inv, Player p) {
 		// closes anyone's inventory who is viewing the container
 		for(HumanEntity h : inv.getViewers()) {
 			h.closeInventory();
